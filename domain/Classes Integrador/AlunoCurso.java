c65f0c87c1a4d0452f4292dea3b18493bfa0f7f9
Pedro Paulo Boveto <pedroboveto@gmail.com>
2015-04-22 20:44:22 -0300

    classe alunocurso

---
+++ domain/Classes Integrador/AlunoCurso.java
@@ -0,0 +1,19 @@
+
+import java.math.BigInteger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pedro
+ */
+public class AlunoCurso {
+    private BigInteger idAlunoCurso;
+    private Aluno idAluno;
+    private Curso idCurso;
+
+}
